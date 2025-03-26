2b337fdb766a9772be78b371546034ff734293a7
Horacio Duran <horacio.duran@gmail.com>
2025-03-26 09:09:54 +0100

    Ran ruff check

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,12 +1,10 @@
 from .verify import DefaultUsedAsError, EnvironmentVariableError, RequiredVariableMissingError, must_pass_check
 
 __all__ = [
-
     # Verify functions
     "must_pass_check",
-
     # Exceptions
     "EnvironmentVariableError",
     "RequiredVariableMissingError",
-    "DefaultUsedAsError"
+    "DefaultUsedAsError",
 ]
