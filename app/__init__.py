7a81cb5e31b3464400d87006e010f4e79aec2717
Horacio Duran <horacio.duran@gmail.com>
2025-03-26 11:51:43 +0100

    update __all__ for app module

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,3 @@
-from .app import main
+from .app import main, update_env_var_catalogue, process_individual_var, print_structured, check_env_vars
 
-__all__ = ["main"]
+__all__ = ["main", "update_env_var_catalogue", "process_individual_var", "print_structured", "check_env_vars"]
